1abc2f7ffe4f39afaf079220a9e8f1733ca20f00
“lion” <narashimha.sk@gmail.com>
2019-03-30 19:18:16 +0530

    commite

---
+++ src/com/sample/applicaton/git/Client.java
@@ -0,0 +1,13 @@
+
+package com.sample.applicaton.git;
+
+public class Client {
+public static void main(String[] args) {
+	System.out.println("client method");
+	System.out.println("Ganesh");
+	
+}
+	
+	
+	
+}
---
+++ src/com/sample/applicaton/git/SampleGit.java
@@ -0,0 +1,10 @@
+package com.sample.applicaton.git;
+
+public class SampleGit {
+	public static void main(String[] args) {
+		System.out.println("Ganesh");
+		System.out.println("Pani");
+		System.out.println("Sardar");
+		System.out.println("Srinu");
+	}
+}
