1d3fbe340da6e860ee40784f31104ac8ac801444
“lion” <narashimha.sk@gmail.com>
2019-03-31 08:04:17 +0530

    my life most powerful persons in my more impact persons never forget these persons in my life

--- src/com/sample/applicaton/git/Client.java
+++ src/com/sample/applicaton/git/Client.java
@@ -5,7 +5,18 @@ public class Client {
 public static void main(String[] args) {
 	System.out.println("client method");
 	System.out.println("Ganesh");
+	System.out.println("Paneedra Simhadri(Lion)");
+	System.out.println("Snumil");
+	System.out.println("Anil");
+	System.out.println("Akshaya");
+	System.out.println("Pushker");
+	System.out.println("Sai");
 	System.out.println("Sardar");
+	System.out.println("Rupesh");
+	System.out.println("Srinu");
+	System.out.println("Prasad");
+	
+	
 	
 }
 	
